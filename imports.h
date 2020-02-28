5241a7b336cb8ec2bff6d50dc916bd6839fbabdf
Benas <benasi89@gmail.com>
2020-02-28 18:54:39 +0200

    Fixing an accidental v0.4 merge

--- imports.h
+++ imports.h
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <iomanip>
 #include <random>
-#include <chrono>
+#include <ctime>
 #include <algorithm>
 
 using std::string;
@@ -15,31 +15,6 @@ using std::setw;
 
 struct stud {
 	string name, lname;
-	float medAvg, avg;
 	int exam;
 	std::vector<int> nd;
 };
-
-class timer{
-	std::chrono::high_resolution_clock::time_point start, end;
-	std::chrono::duration<double> length;
-public:
-	timer() { start =  std::chrono::high_resolution_clock::now(); }
-	void stop() { 
-		end =  std::chrono::high_resolution_clock::now();
-		length = std::chrono::duration_cast<std::chrono::duration<double>>(end-start);
-	}
-	double duration() { return length.count(); }
-};
-
-void CreateInput(int, int);
-void Input(vector<stud>&, string);
-float Final(float, float);
-float Average(stud);
-float Median(stud);
-void AssignGrades(vector<stud>&);
-void Pick(vector<stud>&, vector<stud>&, vector<stud>&);
-void Output(vector<stud>);
-void Output2file(vector<stud>, string);
-bool compareByName(stud, stud);
-string whichFile(int);
